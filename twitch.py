238934fecd4db391c91e860568d519dd3c4c9368
Benjamin <patonacevedo>
2020-08-15 13:50:52 -0400

    some modifications

--- twitch.py
+++ twitch.py
@@ -54,44 +54,51 @@ def pie_chart_lol_views_per_country():
 
     plt.show()
 
+def clean_data():
+    date_times = df_twitch_stream.time.str.split(pat=' ', expand=True)
 
-# US viewers at different hours of the day on January 1st, 2015
+    df_date = date_times[0]
+    date = {'year': pd.DatetimeIndex(df_date).year,
+            'month': pd.DatetimeIndex(df_date).month,
+            'day': pd.DatetimeIndex(df_date).day}
+    date = pd.DataFrame(data=date)
 
-# date_times = df_twitch_stream.time.str.split(pat=' ', expand=True)
-#
-# df_date = date_times[0]
-# date = {'year': pd.DatetimeIndex(df_date).year,
-#         'month': pd.DatetimeIndex(df_date).month,
-#         'day': pd.DatetimeIndex(df_date).day}
-# date = pd.DataFrame(data=date)
-#
-# for key, date_unit in date.items():
-#     df_twitch_stream[key] = date_unit
-#
-# df_time = date_times[1]
-# time = {'hour': pd.DatetimeIndex(df_time).hour,
-#         'min': pd.DatetimeIndex(df_time).minute,
-#         'sec': pd.DatetimeIndex(df_time).second}
-# time = pd.DataFrame(data=time)
-#
-# df_twitch_stream['hour'] = time.hour
-#
-# for key, time_unit in time.items():
-#     df_twitch_stream[key] = time_unit
-# df_twitch_stream.to_csv('cleaner_twitch_stream.csv', index=False)
-jan_15_2015 = df_twitch_stream[(df_twitch_stream.year == 2015) &
-                               (df_twitch_stream.month == 1) &
-                               (df_twitch_stream.day == 1)]
-views_per_hour = jan_15_2015.groupby('hour').hour.agg('count').to_frame('views').reset_index()
-views_per_hour.sort_values(by='hour', ascending=True, inplace=True, ignore_index=True)
-print(views_per_hour)
+    for key, date_unit in date.items():
+        df_twitch_stream[key] = date_unit
 
-ax = plt.subplot()
-plt.title("Views per Hour")
-ax.set_xticks(range(len(views_per_hour.hour)))
-ax.set_xticklabels(views_per_hour.hour)
-plt.plot(views_per_hour.hour, views_per_hour.views)
-plt.xlabel('hour')
-plt.ylabel('views')
+    df_time = date_times[1]
+    time = {'hour': pd.DatetimeIndex(df_time).hour,
+            'min': pd.DatetimeIndex(df_time).minute,
+            'sec': pd.DatetimeIndex(df_time).second}
+    time = pd.DataFrame(data=time)
 
-plt.show()
+    df_twitch_stream['hour'] = time.hour
+
+    for key, time_unit in time.items():
+        df_twitch_stream[key] = time_unit
+    df_twitch_stream.to_csv('cleaner_twitch_stream.csv', index=False)
+
+def lol_views_jan_1():
+
+    jan_15_2015 = df_twitch_stream[(df_twitch_stream.year == 2015) &
+                                   (df_twitch_stream.month == 1) &
+                                   (df_twitch_stream.day == 1) &
+                                   (df_twitch_stream.game == 'League of Legends')]
+    views_per_hour = jan_15_2015.groupby('hour').hour.agg('count').to_frame('views').reset_index()
+    views_per_hour.sort_values(by='hour', ascending=True, inplace=True, ignore_index=True)
+    print(views_per_hour)
+
+    ax = plt.subplot()
+    plt.title("League of Legends Views in Jan,1")
+    ax.set_xticks(range(len(views_per_hour.hour)))
+    ax.set_xticklabels(views_per_hour.hour)
+
+    plt.plot(views_per_hour.hour, views_per_hour.views)
+    y_lower = views_per_hour.views - (views_per_hour.views * 0.15)
+    y_upper = views_per_hour.views + (views_per_hour.views * 0.15)
+    plt.fill_between(range(len(views_per_hour.hour)), y_lower, y_upper, alpha=0.2)
+
+    plt.xlabel('hour')
+    plt.ylabel('views')
+    plt.show()
+
