cecf4e71813e6988771d98118cfa296c82e5a595
Benjamin <patonacevedo>
2020-09-02 17:12:22 -0400

    Added tennis Stats

---
+++ tennis_ace_starting/tennis_ace_starting/script.py
@@ -0,0 +1,122 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+# load and investigate the data here:
+df = pd.read_csv('tennis_stats.csv')
+print(df.head())
+print(df.columns)
+print(df.dtypes)
+
+df.sort_values(by='Year', inplace=True)
+
+
+# perform exploratory analysis here:
+plt.subplot(2,2,1)
+plt.boxplot(df.ReturnPointsWon)
+
+plt.subplot(2, 2, 2)
+plt.scatter(df.Year, df.ReturnPointsWon)
+
+plt.subplot(2, 2, 3)
+plt.bar(df.Year, df.ReturnPointsWon)
+
+plt.clf()
+
+# plt.subplot(2, 2, 2)
+# plt.scatter(df.Player, df.Winnings)
+# plt.xticks(rotation=90)
+#
+# plt.subplot(2, 2, 1)
+# plt.scatter(df.Player, df.BreakPointsOpportunities)
+# plt.xticks(rotation=90)
+
+fsr_points_won = df[['FirstServeReturnPointsWon']]
+winning = df[['Winnings']]
+
+fsr_points_won_train, fsr_points_won_test, winning_train, winning_test = train_test_split(fsr_points_won, winning, train_size=.8)
+
+model = LinearRegression()
+model.fit(fsr_points_won_train, winning_train)
+
+model.score(fsr_points_won_test, winning_test)
+
+prediction = model.predict(fsr_points_won_test)
+
+plt.subplot(2, 1, 1)
+plt.scatter(winning_test, prediction, alpha=.7)
+plt.subplot(2, 1, 2)
+plt.scatter(fsr_points_won_twinning_trinrain, winning_train, alpha=.4)
+
+plt.show()
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## perform single feature linear regressions here:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## perform two feature linear regressions here:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## perform multiple feature linear regressions here:
